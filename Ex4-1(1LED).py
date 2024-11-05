3f6877e3327927229075e248563f62d7b68d98fc
Siddu Ramagiri <86881570+siddharthramagiri@users.noreply.github.com>
2024-11-05 08:17:47 +0530

    Update Ex4-1(1LED).py

--- Ex4-1(1LED).py
+++ Ex4-1(1LED).py
@@ -1,13 +1,12 @@
-# Run a python program on pi to demonstrate file operations
-file_name = "sample.txt"
-# Write to file
-with open(file_name, 'w') as file:
- file.write("Hello, this is a sample file written using Python on Raspberry Pi\n")
-# Read from file
-with open(file_name, 'r') as file:
- content = file.read()
- print("File content:", content)
- 
- 
- 
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(3,GPIO.OUT)
+while True:
+    GPIO.output(3,GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(3,GPIO.LOW)
+    time.sleep(1)
+
+
 # Pins – 3 , 6
